209b4a5f5ab5fe314711e1a3e311f185e19dd1f6
Emilis Petrilionis <Emilis0627@gmail.com>
2021-06-25 21:47:16 +0200

    Main

--- Main.py
+++ Main.py
@@ -1,14 +1,43 @@
 from bottle import route, run, template, request, static_file
+from LK3 import *
 
 
 @route('/')
 def index():
-    return template("website/Sudoku.tpl")
+    grid = [["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""]]
+    return template("website/Sudoku.tpl", grid=grid)
+
+
+@route("/solve")
+def solving():
+    return template("website/Sudoku.tpl", grid=[["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", ""]])
+
+
+@route('/solve', method="POST")
+def solving():
+    grid = solve(request.forms)
+    return template("website/Sudoku.tpl", grid=grid)
 
 
 @route('/static/<filename>')
 def server_static(filename):
-    return static_file(filename, root='static/')
+    return static_file(filename, root='website/static/')
 
 
-run()
+def solve(form):
+    grid = init_empty_grid()
+    print(grid)
+    counter = 0
+    for row_index in range(9):
+        for column_index in range(9):
+            if form.get(f"f{str(counter)}") == "":
+                grid[row_index][column_index] = 0
+            else:
+                print(form.get(f"f{str(counter)}"))
+                grid[row_index][column_index] = int(form.get(f"f{str(counter)}"))
+            counter += 1
+    print(grid)
+    return main(grid)
+
+
+run(debug=True, reloader=True)
